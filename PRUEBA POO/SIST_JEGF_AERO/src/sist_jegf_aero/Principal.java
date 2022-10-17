f52e79f0acd3cfed2679794495e3c66c5519ac1c
Joreims <jorge.garcia14624@est.uca.edu.ni>
2022-10-17 08:36:26 -0600

    Prueba

---
+++ PRUEBA POO/SIST_JEGF_AERO/src/sist_jegf_aero/Principal.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sist_jegf_aero;
+import java.util.ArrayList;
+import java. util. Scanner;
+/**
+ *
+ * @author isi7
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> empleados = new ArrayList<>();
+        System.out.println("Ingrese el primer empleado");
+        String empleado = scanner.next();
+        empleados.add(empleado);
+        System.out.println(empleados);
+
+ 
+        //switch{
+          //   case 1:
+            //    break;
+            // case 2:
+              //  break;
+            // case 3:
+              //   break;
+        }//}
+            
+    
+    
+    
+}
